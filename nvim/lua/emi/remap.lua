d81e7faf057c2024b7c3c58499f5873a9d3280fa
Hector Ledesma <hector.e.ledesma21@gmail.com>
2024-03-02 02:37:41 -0500

    Add ThePrimeagen's remaps from 0 to LSP video

--- nvim/lua/emi/remap.lua
+++ nvim/lua/emi/remap.lua
@@ -1,2 +1,34 @@
-vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+-- move highlighted lines up and down, and indent
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- enhanced J. Cursor stays in place
+vim.keymap.set("n", "J", "mzJ`z")
+-- keeps cursor centered while moving half a page up and down
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+-- searching string matches keeps cursor in the middle of the screen
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+-- paste w/o losing buffer
+vim.keymap.set("x", "<leader>p", "\"_dP")
+
+-- yank to clipboard
+vim.keymap.set("n", "<leader>y", "\"+y")
+vim.keymap.set("v", "<leader>y", "\"+y")
+vim.keymap.set("n", "<leader>Y", "\"+Y")
+
+-- quickfix
+vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
+vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
+vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
+vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
+
+-- Replace all instances of word under cursor
+vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+
+vim.keymap.set("n", "Q", "<nop>")
+
